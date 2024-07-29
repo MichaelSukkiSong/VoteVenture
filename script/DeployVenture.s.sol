8929076e11f47696b312fcb4da060ad481fd4b86
MichaelSukkiSong <sa77irak@gmail.com>
2024-07-29 10:33:16 -0700

    update: deploy script to use various network config

--- script/DeployVenture.s.sol
+++ script/DeployVenture.s.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.26;
 
 import {Script} from "forge-std/Script.sol";
 import {Venture} from "../src/Venture.sol";
+import {NetworkConfig} from "./NetworkConfig.s.sol";
 
 contract DeployVenture is Script {
     function run() external returns (Venture) {
+        NetworkConfig networkConfig = new NetworkConfig();
+        address priceFeedAddress = networkConfig.activeNetworkConfig();
+
         vm.startBroadcast();
-        Venture venture = new Venture();
+        Venture venture = new Venture(priceFeedAddress);
         vm.stopBroadcast();
 
         return venture;
