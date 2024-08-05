ef7459076b20d75cba8cb0d4da01db7ff6e29299
MichaelSukkiSong <sa77irak@gmail.com>
2024-08-05 12:09:06 -0700

    update: deploy script to deploy venture factory

--- contracts/script/DeployVentureFactory.s.sol
+++ contracts/script/DeployVentureFactory.s.sol
@@ -3,18 +3,18 @@
 pragma solidity ^0.8.26;
 
 import {Script} from "forge-std/Script.sol";
-import {Venture} from "../src/Venture.sol";
+import {VentureFactory} from "../src/Venture.sol";
 import {ConfigureConfig} from "./ConfigureConfig.s.sol";
 
-contract DeployVenture is Script {
-    function run() external returns (Venture) {
+contract DeployVentureFactory is Script {
+    function run() external returns (VentureFactory) {
         ConfigureConfig configureConfig = new ConfigureConfig();
         address priceFeedAddress = configureConfig.activeNetworkConfig();
 
         vm.startBroadcast();
-        Venture venture = new Venture(priceFeedAddress);
+        VentureFactory ventureFactory = new VentureFactory(priceFeedAddress);
         vm.stopBroadcast();
 
-        return venture;
+        return ventureFactory;
     }
 }
