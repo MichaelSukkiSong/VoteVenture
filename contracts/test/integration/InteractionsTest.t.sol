36f636a11445a215ec9e604e9dc30e38e17edac8
MichaelSukkiSong <sa77irak@gmail.com>
2024-08-14 01:53:07 +0900

    updatep: updated interactions test with venture factory

--- contracts/test/integration/InteractionsTest.t.sol
+++ contracts/test/integration/InteractionsTest.t.sol
@@ -3,86 +3,118 @@
 pragma solidity ^0.8.26;
 
 import {Test} from "forge-std/Test.sol";
-import {DeployVenture} from "../../script/DeployVenture.s.sol";
-import {Venture} from "../../src/Venture.sol";
-import {FundVenture, CreateRequestVenture, ApproveRequestVenture, FinalizeRequestVenture} from "../../script/Interactions.s.sol";
+import {DeployVentureFactory} from "../../script/DeployVentureFactory.s.sol";
+import {VentureFactory, Venture} from "../../src/Venture.sol";
+import {CreateVenture, FundVenture, CreateRequestVenture, ApproveRequestVenture, FinalizeRequestVenture} from "../../script/Interactions.s.sol";
 
 contract InteractionsTest is Test {
-    Venture venture;
+    VentureFactory ventureFactory;
+    DeployVentureFactory deployVentureFactory;
 
     function setUp() public {
-        DeployVenture deployVenture = new DeployVenture();
-        venture = deployVenture.run();
+        deployVentureFactory = new DeployVentureFactory();
+        ventureFactory = deployVentureFactory.run();
+
+        // address[] memory deployedVentures = ventureFactory
+        //     .getDeployedVentures();
+        // venture = Venture(deployedVentures[0]);
     }
 
-    function test_UserCanFundVenture() public {
-        FundVenture fundVenture = new FundVenture();
-        fundVenture.fundVenture(address(venture));
+    function test_CreateVenture() public {
+        CreateVenture createVenture = new CreateVenture();
+        createVenture.createVenture(address(ventureFactory));
 
-        assertEq(address(venture).balance, fundVenture.FUND_AMOUNT());
+        assertEq(ventureFactory.getDeployedVentures().length, 1);
+    }
+
+    modifier createdVenture() {
+        CreateVenture createVenture = new CreateVenture();
+        createVenture.createVenture(address(ventureFactory));
+        _;
+    }
+
+    function test_UserCanFundVenture() public createdVenture {
+        FundVenture fundVenture = new FundVenture();
+        fundVenture.fundVenture(address(ventureFactory));
+
+        assertEq(
+            ventureFactory.getDeployedVentures()[0].balance,
+            fundVenture.FUND_AMOUNT()
+        );
     }
 
     modifier funded() {
         FundVenture fundVenture = new FundVenture();
-        fundVenture.fundVenture(address(venture));
+        fundVenture.fundVenture(address(ventureFactory));
         _;
     }
 
-    function test_OwnerCanCreateRequest() public funded {
+    function test_OwnerCanCreateRequest() public createdVenture funded {
         CreateRequestVenture createRequestVenture = new CreateRequestVenture();
-        createRequestVenture.createRequestVenture(address(venture));
+        createRequestVenture.createRequestVenture(address(ventureFactory));
 
         assertEq(
-            venture.getRequestDescription(0),
+            Venture(ventureFactory.getDeployedVentures()[0])
+                .getRequestDescription(0),
             createRequestVenture.REQUEST_DESCRIPTION()
         );
         assertEq(
-            venture.getRequestAmount(0),
+            Venture(ventureFactory.getDeployedVentures()[0]).getRequestAmount(
+                0
+            ),
             createRequestVenture.REQUEST_AMOUNT()
         );
         assertEq(
-            venture.getRequestRecipient(0),
+            Venture(ventureFactory.getDeployedVentures()[0])
+                .getRequestRecipient(0),
             createRequestVenture.RECIPIENT()
         );
     }
 
-    modifier fundedAndCreatedRequest() {
-        FundVenture fundVenture = new FundVenture();
-        fundVenture.fundVenture(address(venture));
-
+    modifier createdRequest() {
         CreateRequestVenture createRequestVenture = new CreateRequestVenture();
-        createRequestVenture.createRequestVenture(address(venture));
+        createRequestVenture.createRequestVenture(address(ventureFactory));
         _;
     }
 
-    function test_UserCanApproveRequest() public fundedAndCreatedRequest {
-        uint256 countBeforeApproval = venture.getRequestApprovalCount(0);
+    function test_UserCanApproveRequest()
+        public
+        createdVenture
+        funded
+        createdRequest
+    {
+        uint256 countBeforeApproval = Venture(
+            ventureFactory.getDeployedVentures()[0]
+        ).getRequestApprovalCount(0);
         ApproveRequestVenture approveRequestVenture = new ApproveRequestVenture();
-        approveRequestVenture.approveRequestVenture(address(venture));
-        uint256 countAfterApproval = venture.getRequestApprovalCount(0);
+        approveRequestVenture.approveRequestVenture(address(ventureFactory));
+        uint256 countAfterApproval = Venture(
+            ventureFactory.getDeployedVentures()[0]
+        ).getRequestApprovalCount(0);
 
         assertEq(countAfterApproval, countBeforeApproval + 1);
     }
 
-    modifier fundedAndCreatedRequestAndApprovedRequest() {
-        FundVenture fundVenture = new FundVenture();
-        fundVenture.fundVenture(address(venture));
-
-        CreateRequestVenture createRequestVenture = new CreateRequestVenture();
-        createRequestVenture.createRequestVenture(address(venture));
-
+    modifier approvedRequest() {
         ApproveRequestVenture approveRequestVenture = new ApproveRequestVenture();
-        approveRequestVenture.approveRequestVenture(address(venture));
+        approveRequestVenture.approveRequestVenture(address(ventureFactory));
         _;
     }
 
     function test_OwnerCanFinalizeRequest()
         public
-        fundedAndCreatedRequestAndApprovedRequest
+        createdVenture
+        funded
+        createdRequest
+        approvedRequest
     {
         FinalizeRequestVenture finalizeRequestVenture = new FinalizeRequestVenture();
-        finalizeRequestVenture.finalizeRequestVenture(address(venture));
+        finalizeRequestVenture.finalizeRequestVenture(address(ventureFactory));
 
-        assert(venture.getRequestComplete(0));
+        assert(
+            Venture(ventureFactory.getDeployedVentures()[0]).getRequestComplete(
+                0
+            )
+        );
     }
 }
