84587cc3ba28fa7f0b8430051942af17bb108e93
MichaelSukkiSong <sa77irak@gmail.com>
2024-08-26 08:09:41 +0900

    add: create request page

--- frontend/src/app/ventures/[address]/requests/new/page.tsx
+++ frontend/src/app/ventures/[address]/requests/new/page.tsx
@@ -1,3 +1,12 @@
-export default function Page({ params }: { params: { address: string } }) {
-  return <h1>My Page</h1>;
-}
+import CreateRequestForm from "@/components/venture/request/CreateRequestForm";
+
+const CreateRequestPage = () => {
+  return (
+    <div className="min-h-screen py-16">
+      <h1 className="text-2xl mb-4 font-bold">Create a Request</h1>
+      <CreateRequestForm />
+    </div>
+  );
+};
+
+export default CreateRequestPage;
